e4d661201a4f17359bb3e477a4aad88554520743
Vlad Kurluk <vlad11kurluk89@gmail.com>
2018-09-25 21:49:49 +0300

    Add first application markup (default-Vuetify)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  
+  <v-app>
+    <v-navigation-drawer app></v-navigation-drawer>
+    <v-toolbar app></v-toolbar>
+    <v-content>
+        <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
