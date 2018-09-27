bed86b3596f056b57b70f62ac89649c4388bc615
Vlad Kurluk <vlad11kurluk89@gmail.com>
2018-09-27 22:28:25 +0300

    tied the Login form to the data

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -10,9 +10,10 @@
                 <v-form>
                   <v-text-field 
                     prepend-icon="person" 
-                    name="login" 
-                    label="Login" 
-                    type="text"
+                    name="email" 
+                    label="Email" 
+                    type="email"
+                    v-model="email"
                   ></v-text-field>
                   <v-text-field 
                     id="password" 
@@ -20,12 +21,16 @@
                     name="password" 
                     label="Password" 
                     type="password"
+                    v-model="password"
                   ></v-text-field>
                 </v-form>
               </v-card-text>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="primary">Login</v-btn>
+                <v-btn 
+                  color="primary"
+                  @click="onSubmit"
+                >Login</v-btn>
               </v-card-actions>
             </v-card>
           </v-flex>
@@ -36,7 +41,15 @@
 <script>
 export default {
   data () {
-    return {}
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    onSubmit () {
+      alert('Hi')
+    }
   }
 }
 </script>
