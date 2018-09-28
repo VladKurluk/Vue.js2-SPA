9fa2de3966d465c08ec8bfebfde328288d00ea43
Vlad Kurluk <vlad11kurluk89@gmail.com>
2018-09-28 21:52:06 +0300

    Create markup template for Registration component

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -4,7 +4,7 @@
           <v-flex xs12 sm8 md6 lg4>
             <v-card class="elevation-12">
               <v-toolbar dark color="primary">
-                <v-toolbar-title>Login form</v-toolbar-title>
+                <v-toolbar-title>Login</v-toolbar-title>
               </v-toolbar>
               <v-card-text>
                 <v-form ref="form" v-model="valid" validation>
--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -1,9 +1,53 @@
 <template>
-    <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>Registration</h1>
-            </v-flex>
+    <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md6 lg4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Registration</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form ref="form" v-model="valid" lazy-validation>
+                  <v-text-field 
+                    prepend-icon="person" 
+                    name="email" 
+                    label="Email" 
+                    type="email"
+                    v-model="email"
+                    :rules="emailRules"
+                  ></v-text-field>
+                  <v-text-field 
+                    id="password" 
+                    prepend-icon="lock" 
+                    name="password" 
+                    label="Password" 
+                    type="password"
+                    v-model="password"
+                    :counter='8'
+                    :rules="passwordRules"
+                  ></v-text-field>
+                  <v-text-field 
+                    id="password" 
+                    prepend-icon="security" 
+                    name="confirm-password" 
+                    label="Confirm password" 
+                    type="password"
+                    v-model="confirmPassword"
+                    :counter='8'
+                    :rules="confirmPasswordRules"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn 
+                  color="primary"
+                  @click="onSubmit"
+                  :disabled="!valid"
+                >Create account</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
         </v-layout>
     </v-container>
 </template>
@@ -11,7 +55,35 @@
 <script>
 export default {
   data () {
-    return {}
+    return {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      valid: false,
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => (v && v.length > 8) || 'Password must be less than 8 characters'
+      ],
+      confirmPasswordRules: [
+        v => !!v || 'Password is required',
+        v => v === this.password || 'Password should match'
+      ]
+    }
+  },
+  methods: {
+    onSubmit () {
+      if (this.$refs.form.validate()) {
+        const user = {
+          email: this.email,
+          password: this.password
+        }
+        console.log(user)
+      }
+    }
   }
 }
 </script>
