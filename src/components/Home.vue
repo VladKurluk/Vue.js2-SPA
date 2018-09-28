36b05b7079f1fede3f70d8e72089164252d914bf
Vlad Kurluk <vlad11kurluk89@gmail.com>
2018-09-28 22:22:14 +0300

    Create carousel in home page component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,37 @@
 <template>
-    <v-container>
+  <div>
+    <v-container fluid>
         <v-layout row>
             <v-flex xs12>
-                <h1>Home</h1>
+               <v-carousel>
+                    <v-carousel-item
+                    v-for="(ad,i) in ads"
+                    :key="i"
+                    :src="ad.imageSrc"
+                    ></v-carousel-item>
+                </v-carousel>
             </v-flex>
         </v-layout>
     </v-container>
+    <v-container>
+        <v-layout row>
+            <v-flex xs12>
+                
+            </v-flex>
+        </v-layout>
+    </v-container>
+    </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      ads: [
+        {title: 'FirstAd', description: 'Descr', promo: false, imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg', id: '123'},
+        {title: 'SecondAd', description: 'Descr2', promo: true, imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg', id: '1234'}
+      ]
+    }
   }
 }
 </script>
