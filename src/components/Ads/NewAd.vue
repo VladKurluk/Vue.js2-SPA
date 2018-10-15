119a002282c7541164cccdd6ca7066af9ddefd29
Vlad Kurluk <vlad11kurluk89@gmail.com>
2018-10-15 12:33:33 +0300

    Added form to add advertisements on the page NewAd

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,8 +1,26 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>NewAd</h1>
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text--secondary mb-3">Create a new ad</h1>
+                <v-form ref="form" v-model="valid" validation>
+                  <v-text-field  
+                    name="title" 
+                    label="Ad Title" 
+                    type="text"
+                    v-model="title"
+                    required
+                    :rules="[v => !!v || 'Title is required']"
+                  ></v-text-field>
+                  <v-text-field  
+                    name="description" 
+                    label="Ad description" 
+                    type="text"
+                    multi-line
+                    v-model="description"
+                    :rules="[v => !!v || 'Title is required']"
+                  ></v-text-field>
+                </v-form>
             </v-flex>
         </v-layout>
     </v-container>
