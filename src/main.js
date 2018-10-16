51f11d07dda41c8d85b79161beedfcc4bc51e01b
Vlad Kurluk <vlad11kurluk89@gmail.com>
2018-10-16 21:19:48 +0300

    Addee from project Firebase and initialization him

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 import Vuetify from 'vuetify'
+import * as firebase from 'firebase'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
@@ -15,5 +16,17 @@ new Vue({
   router,
   store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  created () {
+    // Initialize Firebase
+    var config = {
+      apiKey: 'AIzaSyAlCxfQ8eY7GnB4HjxMieBIyOunymdjtw4',
+      authDomain: 'project-ad-vue.firebaseapp.com',
+      databaseURL: 'https://project-ad-vue.firebaseio.com',
+      projectId: 'project-ad-vue',
+      storageBucket: 'project-ad-vue.appspot.com',
+      messagingSenderId: '259835723193'
+    }
+    firebase.initializeApp(config)
+  }
 })
