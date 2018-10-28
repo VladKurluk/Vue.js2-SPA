0759c4a0f5b47b28fdcd8218b19d81c445c4642d
Vlad Kurluk <vlad11kurluk89@gmail.com>
2018-10-28 14:19:36 +0200

    Added check to determine if user is logged in.

--- src/App.vue
+++ src/App.vue
@@ -84,6 +84,9 @@ export default {
   computed: {
     error () {
       return this.$store.getters.error
+    },
+    isUserLoggedIn () {
+      return this.$store.getters.isUserLoggedIn
     }
   },
   methods: {
--- src/store/user.js
+++ src/store/user.js
@@ -54,6 +54,9 @@ export default {
   getters: {
     user (state) {
       return state.user
+    },
+    isUserLoggedIn (state) {
+      return state.user !== null
     }
   }
 }
