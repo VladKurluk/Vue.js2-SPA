218525943bfc1b2fa76fa3f4a1e4f0046f6993e5
VladKurluk <vlad11kurluk89@gmail.com>
2018-11-10 13:35:48 +0200

    Create logic for orders

--- src/store/orders.js
+++ src/store/orders.js
@@ -1,19 +1,54 @@
+import * as firebase from 'firebase'
+
+class Order {
+  constructor (name, phone, adId, done = false, id = null) {
+    this.name = name
+    this.phone = phone
+    this.adId = adId
+    this.done = done
+    this.id = id
+  }
+}
+
 export default {
   state: {
     orders: []
   },
   mutations: {
-    createOrder (state, payload) {
-      state.orders.push(payload)
+    loadOrders (state, payload) {
+      state.orders = payload
     }
   },
   actions: {
-    async createOrder () {
-      await new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve()
-        }, 4000)
-      })
+    async createOrder ({commit}, {name, phone, adId, ownerId}) {
+      const order = new Order(name, phone, adId)
+      commit('clearError')
+      try {
+        await firebase.database().ref(`/users/${ownerId}/orders/`).push(order)
+      } catch (error) {
+        commit('setError', error.message)
+        throw error
+      }
+    },
+    async fetchOrders ({commit, getters}) {
+      commit('setLoading', true)
+      commit('clearError')
+      const resultOrders = []
+      try {
+        const fbVal = await firebase.database().ref(`/users/${getters.user.id}/orders/`).once('value')
+        const orders = fbVal.val()
+        Object.keys(orders).forEach(key => {
+          const order = orders[key]
+          resultOrders.push(
+            new Order(order.name, order.phone, order.adId, order.done, key)
+          )
+        })
+        commit('loadOrders', resultOrders)
+        commit('setLoading', false)
+      } catch (error) {
+        commit('setLoading', false)
+        commit('setError', error.message)
+      }
     }
   },
   getters: {
